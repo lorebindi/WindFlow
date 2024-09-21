6141d98456a20ce3a180454640b9f9b02182dc92
bindi <l.bindi4@studenti.unipi.it>
2024-09-21 13:30:42 +0000

    Added some comments

--- wf/pinning_thread_context.hpp
+++ wf/pinning_thread_context.hpp
@@ -9,15 +9,16 @@ using namespace ff;
 
 namespace wf {
 
+    /**
+     * pinning_thread_context is the shared struct between operators and used for realizing replicas' thread pinning.
+     */
     struct pinning_thread_context {
-        PinningSpinBarrier& barrier;
+        PinningSpinBarrier& barrier; // This spin barrier is used to ensure that threads only start working once all other threads have been pinned correctly.
         bool has_barrier;
-        vector<int>& cores;
+        vector<int>& cores;  // This is the core vector on which the operator replicas will be pinned.
 
         pinning_thread_context(PinningSpinBarrier& b, bool active, vector<int>& c)
             : barrier(b), has_barrier(active), cores(c) {}
     };
-
 }
-
 #endif // PINNING_THREAD_CONTEXT_HPP
